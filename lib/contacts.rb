d65871d83ff6585447ba187c24cced9d135d8e2d
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-19 21:43:27 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |name, details| 
     details.each do |attribute,data| 
       if attribute == :favorite_ice_cream_flavors 
-        data.delete_if {|cream| cream == "strawberry"
+        data.delete_if {|cream| cream == "strawberry"}
       end 
     end 
   end 
